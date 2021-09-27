5f31e64d75dcf6cb693b288f2c2a831c45bac933
Eko199 <50295282+Eko199@users.noreply.github.com>
2021-09-27 16:51:56 +0300

    6.3

---
+++ HW1/6/Main3.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+        double result = 0D;
+        for (int i=n; i > 0; i--) {
+            result += 1.0/i;
+        }
+        System.out.printf("Result is: " + result);
+    }
+}
