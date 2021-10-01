5ad48b11f56be2b86a7e8514c85cc9de0686d544
Eko199 <asusvikp@gmail.com>
2021-10-01 11:34:09 +0300

    Added 5.3 bonus

--- Homework/src/com/company/Main5_3.java
+++ Homework/src/com/company/Main5_3.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 
 public class Main5_3 {
     //RectanglesDetector
+
+    public static boolean isPointInRect(int x, int y, int rectX1, int rectY1, int rectX2, int rectY2) {
+        return (x >= rectX1 && x <= rectX2 && y <= rectY1 && y >= rectY2);
+    }
+
     public static void main(String[] args) {
         int[] rect1 = new int[4]; //upper left x and y, down right x and y
         int[] rect2 = new int[4];
@@ -25,12 +30,16 @@ public class Main5_3 {
         System.out.println("Second rectangle down right y:");
         rect2[3] = sc.nextInt();
 
-        if (rect1[0] <= rect2[0] && rect1[1] >= rect2[1] && rect1[2] >= rect2[2] && rect1[3] <= rect2[3]) {
+        if (isPointInRect(rect2[0], rect2[1], rect1[0], rect1[1], rect1[2], rect1[3]) && isPointInRect(rect2[2], rect2[3], rect1[0], rect1[1], rect1[2], rect1[3])) {
             System.out.println("Second rectangle is contained in first rectangle.");
-        } else if (rect2[0] <= rect1[0] && rect2[1] >= rect1[1] && rect2[2] >= rect1[2] && rect2[3] <= rect1[3]) {
+        } else if (isPointInRect(rect1[0], rect1[1], rect2[0], rect2[1], rect2[2], rect2[3]) && isPointInRect(rect1[2], rect1[3], rect2[0], rect2[1], rect2[2], rect2[3])) {
             System.out.println("First rectangle is contained in second rectangle.");
         } else {
             System.out.println("Neither of the rectangles is contained in the other.");
+            if (isPointInRect(rect2[0], rect2[1], rect1[0], rect1[1], rect1[2], rect1[3]) || isPointInRect(rect2[2], rect2[3], rect1[0], rect1[1], rect1[2], rect1[3]) ||
+                    isPointInRect(rect2[0], rect2[3], rect1[0], rect1[1], rect1[2], rect1[3]) || isPointInRect(rect2[2], rect2[1], rect1[0], rect1[1], rect1[2], rect1[3])) {
+                System.out.println("The rectangles intersect.");
+            } else System.out.println("The rectangles don't intersect.");
         }
     }
 }
