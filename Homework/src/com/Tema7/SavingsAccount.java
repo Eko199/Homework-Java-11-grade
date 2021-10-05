8b92cd77110b328807e314930ba6ebefc05ed706
Eko199 <asusvikp@gmail.com>
2021-10-05 11:24:25 +0300

    Added 7.1

---
+++ Homework/src/com/Tema7/SavingsAccount.java
@@ -0,0 +1,24 @@
+package com.Tema7;
+
+public class SavingsAccount {
+    public String name;
+    public double balance;
+
+    SavingsAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void deposit(int x) {
+        balance += x;
+    }
+
+    public void withdraw(double x) {
+        if (balance < x)  System.out.println("Balance is too low to withdraw " + x);
+        else if (x < 0) System.out.println("You can't withdraw negative amounts.");
+        else {
+            balance -= x;
+            System.out.println("Current balance: " + x);
+        }
+    }
+}
