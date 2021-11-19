9c4ce31565f5f06c15bf16c1ee491378f69fbc74
Eko199 <asusvikp@gmail.com>
2021-11-19 16:32:35 +0200

    Added 13.2

---
+++ Homework/src/RealHomework/Tema13/Problem2.java
@@ -0,0 +1,17 @@
+package RealHomework.Tema13;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Social Security number in the format DDD-DD-DDDD: ");
+        String ssn = sc.nextLine();
+
+        if (ssn.matches("\\d{3}-\\d{2}-\\d{4}"))
+            System.out.println("Valid SSN.");
+        else
+            System.out.println("Invalid SSN.");
+    }
+}
