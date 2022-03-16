6170864a57738dc8f9b271559d484633bce8d994
Eko199 <asusvikp@gmail.com>
2022-03-16 18:48:49 +0200

    Finished Week 24.1

--- Homework/src/RealHomework2/Week24/Problem2.java
+++ Homework/src/RealHomework2/Week24/Problem2.java
@@ -1,7 +1,51 @@
 package RealHomework2.Week24;
 
+import java.util.Stack;
+
 public class Problem2 {
+    public static int operationWeight(char operation) {
+        return switch (operation) {
+            case '^' -> 0;
+            case '*', '/' -> 1;
+            case '+', '-' -> 2;
+            default -> -1;
+        };
+    }
+
     public static String infixToPostfix(String expression) {
-        
+        StringBuilder c = new StringBuilder();
+        Stack<Character> s = new Stack<>();
+
+        for (char character : expression.toCharArray()) {
+            if (Character.isDigit(character)) {
+                c.append(character);
+                continue;
+            }
+            switch (character) {
+                case '(' -> s.push(character);
+                case '+', '-', '*', '/', '^' -> {
+                    while (!s.empty() && operationWeight(s.peek()) != -1 &&
+                            operationWeight(s.peek()) <= operationWeight(character)) {
+                        c.append(s.pop());
+                    }
+                    s.push(character);
+                }
+                case ')' -> {
+                    while (!s.empty() && s.peek() != '(') {
+                        c.append(s.pop());
+                    }
+                    s.pop();
+                }
+            }
+        }
+
+        while (!s.empty() && s.peek() != '(')
+            c.append(s.pop());
+
+        return c.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(infixToPostfix("2*(1+3)"));
     }
 }
