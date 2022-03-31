709f6c4c89382d91603ee5dfe97f2211f512bf0b
Eko199 <asusvikp@gmail.com>
2022-03-31 20:38:07 +0300

    Stuff yet again

---
+++ Homework/src/RealHomework2/Week26/CountKeywords.java
@@ -0,0 +1,38 @@
+package sets;
+
+import java.util.*;
+import java.io.*;
+
+public class CountKeywords {
+	public static void main(String[] args) throws Exception {
+		File file = new File("src/sets/TestHashSet.java");
+		if (file.exists()) {
+			System.out.println("The number of keywords is " + countKeywords(file));
+		} else {
+			System.out.println("File does not exist");
+		}
+
+	}
+
+	public static int countKeywords(File file) throws Exception {
+		// Array of all Java keywords + true, false and null
+		String[] keywordString = { "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class",
+				"const", "continue", "default", "do", "double", "else", "enum", "extends", "for", "final", "finally",
+				"float", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
+				"new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super",
+				"switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while",
+				"true", "false", "null" };
+		Set<String> keywordSet = new HashSet<String>(Arrays.asList(keywordString));
+		int count = 0;
+
+		Scanner input = new Scanner(file);
+		while (input.hasNext()) {
+			String word = input.next();
+			if (keywordSet.contains(word))
+				count++;
+		}
+		input.close();
+		return count;
+		
+	}
+}
---
+++ Homework/src/random/Tony/Tony.java
@@ -0,0 +1,123 @@
+package random.Tony;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.Random;
+
+
+
+
+
+
+
+
+
+    /*
+
+    |                                     |
+    |                                     |
+    |            -------------            |
+    |_________         |         _________|
+    |        |         |        |         |
+    |        |         |        |         |
+    |        |         |        |         |
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Tony extends JFrame {
+    JLabel text;
+    JButton yes;
+    JButton no;
+
+    public static void main(String[] args) {
+        JFrame tony = new Tony();
+    }
+
+    public void wrongOpinion() {
+        Random random = new Random();
+        no.setText("No (wrong)");
+        no.setBounds(random.nextInt(600) + 10, random.nextInt(250) + 250, 200, 69);
+        while (no.getBounds().intersects(yes.getBounds()) || no.getBounds().contains(getMousePosition()))
+            no.setBounds(random.nextInt(600) + 10, random.nextInt(250) + 250, 200, 69);
+    }
+
+    public Tony() {
+        //NEVER
+        super("Tony :)");
+        setLayout(null);
+        setLocationRelativeTo(null);
+        //GONNA
+        text = new JLabel("Are you beautiful?");
+        text.setFont(new Font("Serif", Font.BOLD, 69));
+        text.setHorizontalAlignment(SwingConstants.CENTER);
+        text.setBounds(0, 0, 800, 200);
+        add(text);
+        //GIVE
+        yes = new JButton("Yes");
+        yes.setFont(new Font("Serif", Font.ITALIC, 30));
+        yes.setBounds(169, 300, 120, 69);
+        yes.setBackground(Color.green);
+        //YOU
+        yes.addActionListener(e -> {
+            text.setText("Correct!");
+            remove(yes);
+            remove(no);
+            JLabel ps = new JLabel("PS: Molia ne umirai, ne iskam naistina da umirash");
+            ps.setHorizontalAlignment(SwingConstants.CENTER);
+            ps.setBounds(100, 300, 600, 100);
+            ps.setFont(new Font("Serif", Font.PLAIN, 30));
+            add(ps);
+            try {
+                Desktop.getDesktop().browse(java.net.URI.create("https://youtu.be/dQw4w9WgXcQ"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            getContentPane().setBackground(Color.GREEN);
+            repaint();
+        });
+        //UP
+        no = new JButton("No");
+        no.setFont(new Font("Serif", Font.PLAIN, 30));
+        no.setBounds(500, 300, 120, 69);
+        no.setBackground(Color.red);
+        //NEVER
+        no.addActionListener(e -> wrongOpinion());
+        //GONNA
+        no.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                wrongOpinion();
+            }
+        });
+        //LET
+        add(yes);
+        add(no);
+        //YOU
+        getContentPane().setBackground(Color.white);
+        setLocation(400, 300);
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        //DOWN
+    }
+}
